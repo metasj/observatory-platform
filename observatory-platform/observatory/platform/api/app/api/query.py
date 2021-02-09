59bede0ac30ab04d9eb2bbd077aae3eb776628d3
aroelo <aniek.roelofs@curtin.edu.au>
2021-02-09 08:47:56 +0800

    Update query.py
    Remove '-20201205' suffix when pointing to indices. Removing the suffix ensures that the alias is used which always points to the latest version of that index.

--- observatory-platform/observatory/platform/api/app/api/query.py
+++ observatory-platform/observatory/platform/api/app/api/query.py
@@ -193,7 +193,7 @@ def create_schema():
 #         agg += '_test'
 #     if agg == 'publisher' and subset == 'collaborations':
 #         return "Invalid combination of aggregation (publisher) and subset (collaborations)", 400
-#     index = f"{subset}-{agg}-20201205"
+#     index = f"{subset}-{agg}"
 #
 #     if search_after_no and search_after_text:
 #         search_after = [search_after_no, search_after_text]
@@ -262,7 +262,7 @@ def parse_args() -> Tuple[str, str, str, dict, int, str]:
         agg += '_test'
     if agg == 'publisher' and subset == 'collaborations':
         return '', '', '', {}, 0, ''
-    index = f"{subset}-{agg}-20201205"
+    index = f"{subset}-{agg}"
 
     max_size = 10000
     if limit:
